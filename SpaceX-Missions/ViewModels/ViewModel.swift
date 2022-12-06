1d363a775873a9be9d402ef467e6833ff58de531
Maks Barbaruk <maksbarbaruk@gmail.com>
2022-12-06 18:16:55 +0200

    vmCommit

--- SpaceX-Missions/ViewModels/ViewModel.swift
+++ SpaceX-Missions/ViewModels/ViewModel.swift
@@ -8,9 +8,8 @@
 import Foundation
 
 class ViewModel: ObservableObject {
-    @MainActor @Published var rockets: [Rocket] = []
-    @MainActor @Published var launches: [Launch] = []
-    
+    @Published var rockets: [Rocket] = []
+    @Published var launches: [Launch] = []
     @Published var metrics = Metrics() {
         didSet {
             if let encoded = try? JSONEncoder().encode(metrics) {
@@ -19,11 +18,48 @@ class ViewModel: ObservableObject {
         }
     }
     
-    let dataManager = DataManager()
+    let dataManager: GetDataManager
+    let rocketsURLString: String
+    let launchesURLString: String
     
-    init() {
-        addSubscribers()
+    init(dataManager: GetDataManager = DataManager(),
+         rocketsURLString: String = "https://api.spacexdata.com/v4/rockets",
+         launchesURLString: String = "https://api.spacexdata.com/v4/launches") {
+        
+        self.dataManager = dataManager
+        self.rocketsURLString = rocketsURLString
+        self.launchesURLString = launchesURLString
+        
         setupMetrics()
+        setupDecoder()
+    }
+    
+    func loadData() async throws {
+        let decoder = JSONDecoder()
+        decoder.keyDecodingStrategy = .convertFromSnakeCase
+        decoder.dateDecodingStrategy = .iso8601
+        
+        do {
+            
+            async let rocketData = try await dataManager.loadData(from: rocketsURLString)
+            async let launchesData = try await dataManager.loadData(from: launchesURLString)
+            
+            let temporaryLaunches = try await decoder.decode([Launch].self, from: launchesData)
+            decoder.dateDecodingStrategy = try getLaunchesDecodingStrategy()
+            let temporaryRockets = try await decoder.decode([Rocket].self, from: rocketData)
+            
+            await MainActor.run(body: {
+                self.rockets = temporaryRockets
+                self.launches = temporaryLaunches
+            })
+            
+        } catch {
+            throw error
+        }
+    }
+    
+    func setupDecoder() {
+        
     }
     
     func setupMetrics() {
@@ -36,21 +72,22 @@ class ViewModel: ObservableObject {
         metrics = Metrics()
     }
     
-    func addSubscribers() {
-        Task {
-            for await value in dataManager.$launches.values {
-                await MainActor.run(body: {
-                    self.launches = value
-                })
+    private func getLaunchesDecodingStrategy() throws -> JSONDecoder.DateDecodingStrategy {
+        let formatter = DateFormatter()
+        return .custom { decoder in
+            let container = try decoder.singleValueContainer()
+            let dateString = try container.decode(String.self)
+            
+            formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+            if let date = formatter.date(from: dateString) {
+                return date
             }
-        }
-        
-        Task {
-            for await value in dataManager.$rockets.values {
-                await MainActor.run(body: {
-                    self.rockets = value
-                })
+            formatter.dateFormat = "yyyy-MM-dd"
+            if let date = formatter.date(from: dateString) {
+                return date
             }
+            throw DecodingError.dataCorruptedError(in: container,
+                                                   debugDescription: "Cannot decode date string \(dateString)")
         }
     }
 }
